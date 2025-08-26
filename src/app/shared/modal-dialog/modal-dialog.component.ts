e916089be15b043c77eb7c1e52978c8d410eebf3
Felipe B <felipebatalhanasc@gmail.com>
2025-08-26 13:47:44 -0300

    ref: remove unnecessary code

--- src/app/shared/modal-dialog/modal-dialog.component.ts
+++ src/app/shared/modal-dialog/modal-dialog.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
